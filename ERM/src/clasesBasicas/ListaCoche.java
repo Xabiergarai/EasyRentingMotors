fef644bc4357223152200602669f13ba1989f05a
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2021-10-26 19:08:15 +0200

    Listas

--- ERM/src/clasesBasicas/ListaCoche.java
+++ ERM/src/clasesBasicas/ListaCoche.java
@@ -1,10 +1,39 @@
 package clasesBasicas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ListaCoche extends Coche {
+	
+	private Map<Integer, Coche> coches;
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ListaCoche listaCoche = new ListaCoche();
+		
+		Map<Integer, Coche> lista = listaCoche.getCoches();
+		
+		lista.put(
+				01, 
+				new Coche("CCB", "Audi A3", "ClaseB", "Audi", "2018-07-06", "Gasolina" , 20.0000 , ""));
+				
+				lista.get(01);
+		
+	
+	}
+	
+	public Map<Integer, Coche> getCoches() {
+		
+		return coches;
+	}
+	
+	public void setProductos(Map<Integer, Coche> coches) {
+		this.coches = coches;
+	}
+	
+	public void listaCoche() {
+		this.coches = new HashMap<Integer, Coche>();
+		}
 	}
 
-}
---
+++ ERM/src/clasesBasicas/ListaUsuario.java
@@ -0,0 +1,38 @@
+package clasesBasicas;
+
+import java.util.ArrayList;
+
+public class ListaUsuario {
+	
+	private ArrayList<Usuario> usuarios;
+
+	public static void main(String[] args) {
+
+		
+		ListaUsuario listaUsuario = new ListaUsuario();
+		
+		ArrayList<Usuario> lista = listaUsuario.getUsuarios();
+		lista.add(
+				new Usuario(
+						
+						"Xabier", "Garai Pascual", "XabierG", "Cont123","Xabier.garai@opendeusto.es"));
+				
+			lista.get(0);
+		
+		
+	}
+
+	public ListaUsuario() {
+		this.usuarios = new ArrayList<Usuario>();
+	}
+	
+	public ArrayList<Usuario> getUsuarios() {
+		
+		return usuarios;
+	}
+	
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+}
