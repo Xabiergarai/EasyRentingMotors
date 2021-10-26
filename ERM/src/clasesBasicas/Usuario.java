df6c452fce807cef75768ca9b34c7a70bc21ec98
contre00 <javicoa@opendeusto.es>
2021-10-26 16:44:08 +0200

    clase usuario

--- ERM/src/clasesBasicas/Usuario.java
+++ ERM/src/clasesBasicas/Usuario.java
@@ -1,5 +1,64 @@
 package clasesBasicas;
 
 public class Usuario {
- //cambio
+	
+	
+	private String nombre, apellidos, usuario, contrasenya, email;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContrasenya() {
+		return contrasenya;
+	}
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	public Usuario(String nombre, String apellidos, String usuario, String contrasenya, String email) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.usuario = usuario;
+		this.contrasenya = contrasenya;
+		this.email = email;
+	}
+
+	public Usuario() {
+		super();
+		this.nombre = " ";
+		this.apellidos = "  ";
+		this.usuario = "  ";
+		this.contrasenya = " ";
+		this.email = "  ";
+	
+	
+	}
+	
 }
--- ERM/src/ventanas/VentanaLogIn.java
+++ ERM/src/ventanas/VentanaLogIn.java
@@ -1,8 +1,19 @@
 package ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import clasesBasicas.Usuario;
 
 public class VentanaLogIn extends JFrame {
 	/**
@@ -10,7 +21,17 @@ public class VentanaLogIn extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
+	private JCheckBox cb;
+	private JButton btnIniciarSesion;
+	private JButton btnRegistrarse;
+	public  JTextField tfEmail;
+	private JPasswordField pfContrasenia;
+	private JLabel lbEmail;
+	private JLabel lblContrasenia;
+	private JPanel panelContenidos;
+	private JLabel lb;
+	public Usuario u;
+	private JMenu menuUsuario;
 	
 	
 	public VentanaLogIn() {
@@ -18,6 +39,13 @@ public class VentanaLogIn extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(100, 100, 500, 250);
 		this.setMinimumSize(new Dimension(250, 250));
+		
+		
+		
+		panelContenidos = new JPanel();
+		panelContenidos.setBackground(Color.WHITE);
+		setContentPane(panelContenidos);
+		panelContenidos.setLayout(new BorderLayout(15, 15));
 		
 		
 	}
