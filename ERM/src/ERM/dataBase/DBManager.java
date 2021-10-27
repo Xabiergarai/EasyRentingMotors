8da485da9588b4416e1b6f61448af24a2baf619a
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2021-10-27 10:30:50 +0200

    ,login

--- ERM/src/ERM/dataBase/DBManager.java
+++ ERM/src/ERM/dataBase/DBManager.java
@@ -2,22 +2,30 @@ package ERM.dataBase;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ERM.clasesBasicas.Usuario;
+import ERM.dataBase.*;
+import baseDatos.BDException;
+
 
 
 public class DBManager {
 
 private Connection conn = null; 
+private static Logger logger = Logger.getLogger(DBManager.class.getName());
+
 	
 	//CREAR CONEXION CON BD
 
-	public void connect() throws DBException { 
+	public void connect(String nombre) throws DBException { 
 		try {
 			Class.forName("org.sqlite.JDBC");
-			conn = DriverManager.getConnection("jdbc:sqlite:data/BD2");
+			conn = DriverManager.getConnection("jdbc:sqlite:data/BD2"+ nombre);
 		} catch (ClassNotFoundException e) {
 			throw new DBException("Error cargando el driver de la BD", e);
 		} catch (SQLException e) {
@@ -32,6 +40,77 @@ private Connection conn = null;
 		} catch (SQLException e) {
 			throw new DBException("Error cerrando la conexiÃ³n con la BD", e);
 		}
+	}
+	
+	public static Statement usarCrearTablasBD(Connection con) throws DBException {
+	try {
+		Statement statement = con.createStatement();
+		try {
+			statement.executeUpdate("create table if not exists Usuario " + "(nick string, " + " con string)");
+
+		} catch (SQLException ex) {
+			logger.log(Level.WARNING, "Tabla Usuario ya existente");
+			throw new DBException("Error creando tabla de usuario a la BD", ex);
+
+		}
+	
+	}
+	
+	
+	}
+	public static int existeUsuario(String nick, String contrasenia) throws BDException {
+		Connection con = connect("videoclub.sqlite3");
+		String sql = "SELECT * FROM Usuario WHERE nick='" + nick + "'";
+		logger.log(Level.INFO, "Seleccionando usuario: " + nick);
+		Statement st = null;
+		ResultSet rs = null;
+		int resultado = 0;
+		try {
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+			if (!rs.next()) {
+				resultado = 0;
+				logger.log(Level.WARNING, "Usuario no exixtente");
+
+			} else {
+				String c = rs.getString(2);
+				if (c.equals(contrasenia)) {
+					resultado = 2;
+					logger.log(Level.FINE, "Usuario existente");
+
+				} else {
+					resultado = 1;
+					logger.log(Level.WARNING, "Contraseña incorrecta");
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new BDException("Error al mirar si existe usuarios en BD", e);
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (st != null)
+				try {
+					st.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return resultado;
 	}
 	
 	//REGISTRAR NUEVO USUARIO
--- ERM/src/ERM/ventanasPrimarias/VentanaLogIn.java
+++ ERM/src/ERM/ventanasPrimarias/VentanaLogIn.java
@@ -2,18 +2,30 @@ package ERM.ventanasPrimarias;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+
 import ERM.clasesBasicas.Usuario;
+import ERM.dataBase.*;
 
 public class VentanaLogIn extends JFrame {
 	/**
@@ -21,24 +33,129 @@ public class VentanaLogIn extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private JCheckBox cb;
-	private JButton btnIniciarSesion;
-	private JButton btnRegistrarse;
-	public  JTextField tfEmail;
-	private JPasswordField pfContrasenia;
-	private JLabel lbEmail;
-	private JLabel lblContrasenia;
-	private JLabel lb;
-	public Usuario u;
-	private JMenu menuUsuario;
+	
+	private JPanel panelCentro, panelBase, panelBotonera ;
+	private JTextField txtNombre;
+	private JPasswordField txtContrasenia;
+	private JLabel usuario, contrasenia;
+	private JButton btnEntrar, btnSalir, btnRegistrar, btnAdmin, btnFavoritos;
+	public static String nick;
 	
 	
-	public VentanaLogIn() {
-		this.setTitle("Easy Renting Motors");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 616, 423);
-		this.setMinimumSize(new Dimension(250, 250));
+		public VentanaLogIn() throws DBException{
+			super();
+			
+			
+			setSize(500,250);
+			setTitle("Inicio de sesion");
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			//setExtendedState(MAXIMIZED_BOTH);
+			
+			//panelCentro = PanelFondo
+			panelCentro.setLayout(new BoxLayout(panelCentro, BoxLayout.Y_AXIS));
+			panelCentro.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10, true));
+			
+			
+			panelBase = new JPanel();
+			panelBase.setBackground(Color.DARK_GRAY);
+			    
+			panelBotonera = new JPanel();
+			panelBotonera.setBackground(Color.DARK_GRAY);
+			    
+			getContentPane().add(panelCentro,BorderLayout.CENTER);
+			getContentPane().add(panelBase, BorderLayout.NORTH);
+			getContentPane().add(panelBotonera, BorderLayout.SOUTH);
+			
+			
+			//usuario = new JLabel();
+			//usuario.setText("Introduzca el nombre de usuario");
+			
+			txtNombre = new JTextField();
+			txtNombre.setPreferredSize(new Dimension (200, 50));
+			posicionaLinea(panelCentro, "Introduzca el usuario", txtNombre);
+			
+			//contrasenia = new JLabel();
+			//contrasenia.setText("Introduzca la contrasenia");
+			
+			txtContrasenia = new JPasswordField();
+			txtContrasenia.setPreferredSize(new Dimension (200, 50));
+			posicionaLinea(panelCentro, "Introduzca la contrasenia", txtContrasenia);
+			
+			 
+			
+			btnEntrar = new JButton();
+			btnEntrar.setToolTipText("A�ade los parametros pedidos y pulsa el boton");
+			btnEntrar.setText("Iniciar sesion");
+			panelBotonera.add(btnEntrar);
+			
+			btnSalir = new JButton();
+			btnSalir.setToolTipText("Pulsa para salir");
+			btnSalir.setText("Salir");
+			panelBotonera.add(btnSalir);
+			
+			btnRegistrar = new JButton();
+			btnRegistrar.setToolTipText("Pulsa para registrarte");
+			btnRegistrar.setText("Registro");
+			btnRegistrar.setVisible(false);
+			panelBotonera.add(btnRegistrar);
+				
+			
+			
+			btnAdmin = new JButton();
+			btnAdmin.setToolTipText("Administracion");
+			btnAdmin.setText("Admin");
+			panelBotonera.add(btnAdmin);
+			
+			btnEntrar.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+					String nick = txtNombre.getText();
+					String contrasenia = txtContrasenia.getText();				
+					int resultado;
+					try {
+						resultado = BDManager.existeUsuario(nick, contrasenia);
+						if(resultado == 2) {
+							JOptionPane.showMessageDialog(null, "BIENVENIDO AL VIDEOCLUB");
+							
+							
+							//new VentanaCliente(nick);
+							//new Ventana();
+							
+						}else if(resultado == 1) {
+							JOptionPane.showMessageDialog(null, "La contrase�a no es correcta", "ERROR!", JOptionPane.ERROR_MESSAGE);
+						}else {
+							JOptionPane.showMessageDialog(null, "Para poder acceder, primero tienes que registrarte");
+							btnRegistrar.setVisible(true);
+
+						}
+						//vaciarCampos();
+					} catch (BDException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					
+				}
+			});
+			
+			
+			
+		}
 		
-		
+			private void posicionaLinea(Container cont, String etiqueta, Component campo) {
+				JPanel tempPanel = new JPanel();
+				tempPanel.setOpaque(false);
+				tempPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); //se hace un flow ajustado a la izquierda
+				JLabel l = new JLabel(etiqueta);
+				l.setPreferredSize(new Dimension(250, 50));
+				l.setFont(new Font(Font.SANS_SERIF,Font.BOLD,18));
+				l.setForeground(Color.WHITE);
+				tempPanel.add(l);
+				tempPanel.add(campo);
+				cont.add(tempPanel);
+			}
 	}
-}
+
