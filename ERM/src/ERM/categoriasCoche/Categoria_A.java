a4c47e1531651f0de9fb25af171a1487422e73df
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2021-10-28 16:15:45 +0200

    Correciones de las clases categoria A y C + creación test categoria A

--- ERM/src/ERM/categoriasCoche/Categoria_A.java
+++ ERM/src/ERM/categoriasCoche/Categoria_A.java
@@ -8,39 +8,49 @@ public class Categoria_A extends Coche{
 	private static final long serialVersionUID = 1L;
 	private int numPuertas;
 	private boolean maletero;
-	
-	public Categoria_A(int numPuertas, boolean maletero) {
-		super();
+
+	public Categoria_A(String id, String nombre, String clase, String marca, String fecha_matriculacion,
+			String combustible, double precio, String rutaFoto, int numPuertas, boolean maletero) {
+		super(id, nombre, clase, marca, fecha_matriculacion, combustible, precio, rutaFoto);
 		this.numPuertas = numPuertas;
 		this.maletero = maletero;
 	}
+	
 	public Categoria_A() {
+		super();
 		this.numPuertas = 0;
 		this.maletero = false;
 	}
-		
+
 	public int getNumPuertas() {
 		return numPuertas;
 	}
+
 	public void setNumPuertas(int numPuertas) {
 		this.numPuertas = numPuertas;
 	}
+
 	public boolean isMaletero() {
 		return maletero;
 	}
+
 	public void setMaletero(boolean maletero) {
 		this.maletero = maletero;
 	}
-	
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Override
 	public String toString() {
 		return "Categoria_A [numPuertas=" + numPuertas + ", maletero=" + maletero + "]";
 	}
-	
-	
-	
-	
-	
-	 
 
+	
+	
+	
+	
 }
+	
+	
