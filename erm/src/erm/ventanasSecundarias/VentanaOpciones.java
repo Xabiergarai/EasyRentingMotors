029b5417142eaf4445a798a224a23403f35b8bc2
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2021-11-30 13:20:01 +0100

    Update VentanaOpciones.java

--- erm/src/erm/ventanasSecundarias/VentanaOpciones.java
+++ erm/src/erm/ventanasSecundarias/VentanaOpciones.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -83,6 +84,9 @@ btnAlquilar.addActionListener(new ActionListener() {
 		try {
 			new VentanaAlquiler();
 		} catch (DBException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
