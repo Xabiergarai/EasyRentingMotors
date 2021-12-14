3f3ba1fc40881976d21e24cc9a32c599f4b1f14f
contre00 <javicoa@opendeusto.es>
2021-12-14 13:38:21 +0100

    nuevas clases para tests

---
+++ erm/test/test/ListaUsuarioTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import erm.clasesBasicas.ListaUsuario;
+
+class ListaUsuarioTest {
+	
+	private ListaUsuario lu;
+
+	@BeforeEach
+	void setUplistaUsuario() {
+		lu = new ListaUsuario();
+	}
+	
+}
