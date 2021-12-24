19dc8513b6244aad09d95dca7c4b2d79455d43c0
gutiaitor6 <aitorgg@opendeusto.es>
2021-12-24 12:58:23 +0100

    id para usuario

--- erm/test/test/UsuarioTest.java
+++ erm/test/test/UsuarioTest.java
@@ -24,13 +24,30 @@ public class UsuarioTest  {
 		 u=new Usuario("Javier", "Contreras", "j.contre", "123", "javicoa@opendeusto.es");
 		 u1=new Usuario();
 	}				
-	
+
+		
+	@Test
+	public void testGetNomUsuario() {
+		assertEquals("j.contre", u.getNomUsuario());
+	}
 	
 	@Test
-	public void testToString() {
-		assertEquals("Usuario [nombre=Javier, apellidos=Contreras, nickname=j.contre, contrasenya=123, email=javicoa@opendeusto.es]", u.toString());
+	public void testSetNomUsuario() {
+		u.setNomUsuario("Xabi00");
+		assertEquals("Xabi00", u.getNomUsuario());
 	}
-					
+	
+	@Test
+	public void testGetNombre() {
+		assertEquals("Javier", u.getNombre());
+	}
+	
+	@Test
+	public void testSetNombre() {
+		u.setNombre("Xabi");
+		assertEquals("Xabi", u.getNombre());
+	}
+				
 	@Test
 	public void testgetApellidos() {
 		assertEquals("Contreras", u.getApellidos());
@@ -64,25 +81,11 @@ public class UsuarioTest  {
 		assertEquals("xabigarai@opendeusto.es", u.getEmail());
 	}
 	
+
 	@Test
-	public void testGetNomUsuario() {
-		assertEquals("j.contre", u.getNomUsuario());
+	public void testToString() {
+		assertEquals("Usuario [nombre=Javier, apellidos=Contreras, nickname=j.contre, contrasenya=123, email=javicoa@opendeusto.es]", u.toString());
 	}
+		
 	
-	@Test
-	public void testSetNomUsuario() {
-		u.setNomUsuario("Xabi00");
-		assertEquals("Xabi00", u.getNomUsuario());
-	}
-	
-	@Test
-	public void testGetNombre() {
-		assertEquals("Javier", u.getNombre());
-	}
-	
-	@Test
-	public void testSetNombre() {
-		u.setNombre("Xabi");
-		assertEquals("Xabi", u.getNombre());
-	}
 }
