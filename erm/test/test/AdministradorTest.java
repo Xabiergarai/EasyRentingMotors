ff108ac01bb39d1ddd9a9e015113c010869802ac
contre00 <javicoa@opendeusto.es>
2021-12-29 13:20:24 +0100

    nuevos tests

--- erm/test/test/AdministradorTest.java
+++ erm/test/test/AdministradorTest.java
@@ -2,13 +2,33 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import erm.clasesBasicas.Administrador;
+import erm.clasesBasicas.Alquiler;
 
 class AdministradorTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Administrador a;
+	private Administrador a1;
+
+	
+	@BeforeEach
+	public void setUp() {
+		 a = new Administrador("j.c", "12", "j@gmail.com", "javier", "contreras", "182.255.255.255", "12", "1", "2", "2");
+		 a1 = new Administrador();
 	}
 
+	@Test
+	public void testSetAdminUsuario() {
+		a.setAdminUsuario("100");
+		assertEquals("100", a.getAdminUsuario());
+	}
+	@Test
+	public void testGetAdminUsuario() {
+		assertEquals("12", a.getAdminUsuario());
+	}
+	
+	
 }
