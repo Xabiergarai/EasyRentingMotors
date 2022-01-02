35dfb220003c3b61b0ed8fb1a46e92950d0c607e
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2022-01-02 14:14:38 +0100

    Create Carrito.java

---
+++ erm/src/erm/clasesBasicas/Carrito.java
@@ -0,0 +1,48 @@
+package erm.clasesBasicas;
+
+public class Carrito {
+private String id;
+private String nombre;
+private double precio;
+private String fecha;
+public Carrito(String id, String nombre, double precio, String fecha) {
+	super();
+	this.id = id;
+	this.nombre = nombre;
+	this.precio = precio;
+	this.fecha = fecha;
+}
+public Carrito() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+public String getFecha() {
+	return fecha;
+}
+public void setFecha(String fecha) {
+	this.fecha = fecha;
+}
+@Override
+public String toString() {
+	return "Carrito [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", fecha=" + fecha + "]";
+}
+
+}
