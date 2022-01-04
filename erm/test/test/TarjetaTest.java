118f4c48f7e94f3a82652356090ebd00cf115886
contre00 <javicoa@opendeusto.es>
2022-01-04 22:16:22 +0100

    test

--- erm/test/test/TarjetaTest.java
+++ erm/test/test/TarjetaTest.java
@@ -44,13 +44,123 @@ class TarjetaTest {
 	
 	@Test
 	public void testGetNumeroTarjeta() {	
-		assertEquals(1, t.getNumeroTarjeta());		
+		assertEquals("1", t.getNumeroTarjeta());		
 	}
 	
 	@Test
 	public void testSetNumeroTarjeta() {
 		t.setNumeroTarjeta("3");
 		assertEquals("3", t.getNumeroTarjeta());
+	}
+	
+	@Test
+	public void testGetCodigoDeSeguridad() {	
+		assertEquals("1", t.getCodigoDeSeguridad());		
+	}
+	
+	@Test
+	public void testSetCodigoDeSeguridad() {
+		t.setCodigoDeSeguridad("3");
+		assertEquals("3", t.getCodigoDeSeguridad());
+	}
+	
+	@Test
+	public void testGetidTarjeta() {	
+		assertEquals(0, t.getidTarjeta());		
+	}
+	
+	@Test
+	public void testSetidTarjeta() {
+		t.setidTarjeta(4);
+		assertEquals(4, t.getidTarjeta());
+	}
+	
+	@Test
+	public void testGetidUsuario() {	
+		assertEquals(1, t.getidUsuario());		
+	}
+	
+	@Test
+	public void testSetidUsuario() {
+		t.setidUsuario(5);
+		assertEquals(5, t.getidUsuario());
+	}
+	
+	@Test
+	public void testGetCodigoPostal2() {	
+		assertEquals("2", t.getCodigoPostal2());		
+	}
+	
+	@Test
+	public void testSetCodigoPostal2() {
+		t.setCodigoPostal2("3");
+		assertEquals("3", t.getCodigoPostal2());
+	}
+	
+	@Test
+	public void testGetNombreCompleto() {	
+		assertEquals( "Javi Contre", t.getNombreCompleto());		
+	}
+	
+	@Test
+	public void testSetNombreCompleto() {
+		t.setNombreCompleto("Javier");
+		assertEquals("Javier", t.getNombreCompleto());
+	}
+	
+	@Test
+	public void testGetDireccion() {	
+		assertEquals( "getxo", t.getDireccion());		
+	}
+	
+	@Test
+	public void testSetDireccion() {
+		t.setDireccion("vitoria");
+		assertEquals("vitoria", t.getDireccion());
+	}
+	
+	@Test
+	public void testGetCodigoPostal() {	
+		assertEquals( "488930", t.getCodigoPostal());		
+	}
+	
+	@Test
+	public void testSetCodigoPostal() {
+		t.setCodigoPostal("48");
+		assertEquals("48", t.getCodigoPostal());
+	}
+	
+	@Test
+	public void testGetCiudad() {	
+		assertEquals( "bilbao", t.getCiudad());		
+	}
+	
+	@Test
+	public void testSetCiudad() {
+		t.setCiudad("vitoria");
+		assertEquals("vitoria", t.getCiudad());
+	}
+	
+	@Test
+	public void testGetEstadoProvincia() {	
+		assertEquals( "bizkaia", t.getEstadoProvincia());		
+	}
+	
+	@Test
+	public void testSetEstadoProvincia() {
+		t.setEstadoProvincia("alava");
+		assertEquals("alava", t.getEstadoProvincia());
+	}
+	
+	@Test
+	public void testGetLineaSegundaDireccion() {	
+		assertEquals( "getxo", t.getLineaSegundaDireccion());		
+	}
+	
+	@Test
+	public void testSetLineaSegundaDireccion() {
+		t.setLineaSegundaDireccion("areeta");
+		assertEquals("areeta", t.getLineaSegundaDireccion());
 	}
 	
 }
