176458ee41b0557a3cc67ce2de811f412afeed7f
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-01-08 09:16:42 +0100

    Hilo compra

--- erm/src/erm/ventanasSecundarias/VentanaPago.java
+++ erm/src/erm/ventanasSecundarias/VentanaPago.java
@@ -160,6 +160,9 @@ public class VentanaPago extends javax.swing.JFrame {
 		BotonGuardar.setForeground(new Color(0, 0, 0));
 		BotonGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				VentanaTransaccionCompra vt = new VentanaTransaccionCompra();
+				setVisible(false);
+				vt.setVisible(true);
 			}
 		});
 		BotonGuardar.setFont(new Font("Tahoma", Font.BOLD, 16));
